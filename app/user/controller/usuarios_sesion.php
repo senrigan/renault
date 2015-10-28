f0d60ce022537e32f36e261bea66e400eeb54938
gilberto cordero <gilberto_1_0@hotmail.com>
2015-10-28 03:13:24 -0600

    login

--- app/user/controller/usuarios_sesion.php
+++ app/user/controller/usuarios_sesion.php
@@ -1,12 +1,49 @@
 <?php
+	include "../../admin/model/DatabaseConector.php";
+
 	session_start();
+	
 	//require "funciones/funciones.php";
 	//$con=conecta();
 	
-	$email=$_REQUEST['user'];
+	$user=$_REQUEST['user'];
 	$pass=$_REQUEST['pass'];
-	echo 1;
+
+	$conector=new DatabaseConector("pruebas","admin","renault","localhost","5432");
+	$conexion=$conector->openConexion();
+	//$user=trim($user);
+	//$pass=trim($pass);
+	$query="SELECT * FROM cuentas WHERE user='$user' AND password='$pass' ";
+	echo $query;
+	$resultado=$conector->executeQueryDefine($query,$conexion);
+
+	$num=pg_num_rows($resultado);
+	if($num==0){
+		//usuario  invalido
+		echo 0;
+	}else{
+		$filas = pg_fetch_all($resultado);
+		$conector->closeConexionDef($conexion);
+		$userName=$filas[0]['user'];
+		$idUser=$filas[0]['id'];
+		$_SESSION['id_user']=$id_user;
+		$_SESSION['userName']=$userName;
+		echo 1;
+	}
 	
+	/*if(!$resultado){
+		echo "ocurrio un error al consultar la base de datos";
+		exit;
+	}else{
+		
+		$sizeElement=sizeof($filas);
+		for($i=0;$i<$sizeElement;$i++){
+
+		}
+	}
+	echo 1;
+	*/
+
 	/*
 	$pass_txt = md5($pass);
 	$sql="SELECT * FROM usuarios WHERE email='$email' AND pass='$pass_txt' AND activo='1' AND eliminado='0' ";
