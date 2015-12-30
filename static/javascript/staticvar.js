d8ecc1b239442bd48efa3aa13bc7849908b454eb
senri <gilberto_1_0@hotmail.com>
2015-12-30 15:22:18 -0600

    cambios

--- static/javascript/staticvar.js
+++ static/javascript/staticvar.js
@@ -1,8 +1,13 @@
-var admin="../../admin/";
+var localurl=location.href;
+localurl=localurl.replace("#",'');
+localurl=localurl.substring(0,localurl.indexOf("/app/"));
+localurl=localurl+"/app/";
+
+var admin=localurl+"admin/";
 var adminview=admin+"view/";
 var adminmodel=admin+"model/";
 var admincontroller=admin+"controller/";
-var user="../../user/";
+var user=localurl+"user/";
 var userview=user+"view/";
 var usermodel=user+"model/";
 var usercontroller=user+"controller/";
