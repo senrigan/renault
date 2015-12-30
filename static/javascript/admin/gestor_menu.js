a3a18aacb74443b59ff4b6b62deb0511a9e87c31
senri <gilberto_1_0@hotmail.com>
2015-12-30 15:26:53 -0600

    cambios

--- static/javascript/admin/gestor_menu.js
+++ static/javascript/admin/gestor_menu.js
@@ -10,18 +10,14 @@ function loadContent(page){
 
 $(document).ready(function() // or $(function()
  {
- 	var localurl=location.href;
-
- 	localurl=localurl.replace("#",'');
-    localurl=localurl.substring(0,localurl.indexOf("/app/"));
-    localurl=localurl+"/app/";
+ 	
      $("#regtec").click(function () {
-    var loadUrl =localurl+"view/"+"registro_tecnico.html";
+    var loadUrl =adminview+"registro_tecnico.html";
     $("#container").load(loadUrl);
 	});
 
 	$("#modtec").click(function () {
-	    var loadUrl = localurl+"view/"+"modificar_tecnico.php";
+	    var loadUrl = adminview+"modificar_tecnico.php";
 	    $("#container").load(loadUrl);
 	});
  });
